7c10fded4784e7fe7cdb2bcc0fb41b8b66619950
noyz <mahdian.navid@gmail.com>
2019-10-24 15:51:13 +0330

    parsed file- point in polygon tested

--- HW1/main.js
+++ HW1/main.js
@@ -1,12 +1,20 @@
+'use strict';
 const turf = require('@turf/turf');
-var pt = turf.point([-77, 44]);
-var poly = turf.polygon([[
+const fs = require('fs');
+let pt = turf.point([-77, 44]);
+let poly = turf.polygon([[
   [-81, 41],
   [-81, 47],
   [-72, 47],
   [-72, 41],
   [-81, 41]
 ]]);
- 
-let b =turf.booleanPointInPolygon(pt, poly);
+let input_data = fs.readFileSync('input.json','utf8');
+let input_data_parsed = JSON.parse(input_data);
+let input_data_features = input_data_parsed['features'];
+let coordinates_zero = input_data_features[0].geometry.coordinates;
+console.log(coordinates_zero);
+//console.log(typeof input_data_parsed);
+
+let b =turf.booleanPointInPolygon(pt, turf.polygon(coordinates_zero));
 console.log(b);
