dc77c180b5911e39d3f0f0506343ee5e5f76eff9
noyz <mahdian.navid@gmail.com>
2019-10-25 10:10:15 +0330

    cleaned-formatted polygons-put response

--- HW1/main.js
+++ HW1/main.js
@@ -1,51 +1,36 @@
-'use strict';
-//const turf = require('@turf/turf');
-import turf from '@turf/turf'
-//const fs = require('fs');
-import fs from 'fs';
-function check_point_in_polygon(point, feature_array){
-    let selected_regions = []
-    feature_array.forEach(feature => {
-      let this_polygon = feature.geometry.coordinates;
-      let aa = turf.polygon(this_polygon);
-      let lat = parseInt(point.lat);
-      let long = parseInt(point.long);
-      let turf_point = turf.point([point.lat,point.long]);
-        let is_point_in_polygon = turf.booleanPointInPolygon(turf_point, turf.polygon(this_polygon));
-        if(is_point_in_polygon){
-          //console.log( "this is aaaaa" + aa);
-          //console.log(this_polygon);
-          selected_regions.push(feature);
-          //console.log(this_polygon + " printed!!")
-        }
-        
-    });
-    //console.log(selected_regions);
-    return selected_regions;
+"use strict";
+import turf from "@turf/turf";
+import fs from "fs";
+export { check_point_in_polygon, read_polygons_from_file, polygon_name_extractor };
+
+function polygon_name_extractor(regions){
+  let polygons_out ={polygons : []}
+  regions.forEach(region => {
+    polygons_out.polygons.push(region.properties.name);
+  });
+  return polygons_out;
 }
-export {check_point_in_polygon, read_polygons_from_file};
-function read_polygons_from_file(){
-  let input_data = fs.readFileSync('input.json','utf8');
+function check_point_in_polygon(point, feature_array) {
+  let selected_regions = [];
+  feature_array.forEach(feature => {
+    let this_polygon = feature.geometry.coordinates;
+    let aa = turf.polygon(this_polygon);
+    let lat = parseInt(point.lat);
+    let long = parseInt(point.long);
+    let turf_point = turf.point([point.lat, point.long]);
+    let is_point_in_polygon = turf.booleanPointInPolygon(
+      turf_point,
+      turf.polygon(this_polygon)
+    );
+    if (is_point_in_polygon) {
+      selected_regions.push(feature);
+    }
+  });
+  return selected_regions;
+}
+function read_polygons_from_file() {
+  let input_data = fs.readFileSync("input.json", "utf8");
   let input_data_parsed = JSON.parse(input_data);
-  let input_data_features = input_data_parsed['features'];
-  //let coordinates_zero = input_data_features[0].geometry.coordinates;
+  let input_data_features = input_data_parsed["features"];
   return input_data_features;
 }
-// let pt = turf.point([-77, 44]);
-// let poly = turf.polygon([[
-//   [-81, 41],
-//   [-81, 47],
-//   [-72, 47],
-//   [-72, 41],
-//   [-81, 41]
-// ]]);
-// let input_data = fs.readFileSync('input.json','utf8');
-// let input_data_parsed = JSON.parse(input_data);
-// let input_data_features = input_data_parsed['features'];
-// console.log(input_data_features)
-// let coordinates_zero = input_data_features[0].geometry.coordinates;
-// console.log(coordinates_zero);
-// console.log(typeof input_data_parsed);
-
-// let b =turf.booleanPointInPolygon(pt, turf.polygon(coordinates_zero));
-// console.log(b);
--- HW1/srvr.js
+++ HW1/srvr.js
@@ -1,36 +1,31 @@
-import {check_point_in_polygon, read_polygons_from_file} from "./main.js";
+import { check_point_in_polygon, read_polygons_from_file, polygon_name_extractor } from "./main.js";
 import bodyParser from "body-parser";
-//const express = require('express');
-import express from 'express';
-const app = express()
-const port = 3000
-let features = read_polygons_from_file();// should be changed to put request
+import express from "express";
+const app = express();
+const port = 3000;
+let features = read_polygons_from_file(); // should be changed to put request
 console.log(features);
-  app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-  }));
-  app.use(bodyParser.json());
-app.get('/', function (req, res) {
-    console.log(req);
-    let query = req.query;
-    let lat = query.lat;
-    let long = query.long;
-    let point = {lat :lat,
-      long: long
-    };
-    let selected_regions = check_point_in_polygon(point,features);
-    console.log(selected_regions);
-    res.send(selected_regions);
   })
-  app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+);
+app.use(bodyParser.json());
+app.get("/", function(req, res) {
+  console.log(req);
+  let query = req.query;
+  let lat = query.lat;
+  let long = query.long;
+  let point = { lat: lat, long: long };
+  let selected_regions = check_point_in_polygon(point, features);
+  let names_to_send = polygon_name_extractor(selected_regions);
+  console.log(names_to_send);
+  res.send(names_to_send);
+});
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-
-
-app.put('/', function (req,res){
-  let body = (req.body);
+app.put("/", function(req, res) {
+  let body = req.body;
   console.log(body);
-  features.push(body)
-  console.log("*****************");
-  console.log(features);
-  //res.send(body);
-})
+  res.send(features);
+});
